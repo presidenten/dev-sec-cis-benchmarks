fa799140f36541920fcbf4fd356e7ee00a9bd96d
Johan Hernefeldt <johan.hernefeldt@teliacompany.com>
2019-11-11 14:01:05 +0100

    Added console output

--- app/run.sh
+++ app/run.sh
@@ -4,6 +4,9 @@ cp /share/ssh/* ~/.ssh/
 eval `ssh-agent -s` > /dev/null 2>&1
 ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
 
-./inspect.sh kubernetes
-./inspect.sh docker
+echo "  - Checking dil compliance..."
 ./inspect.sh dil
+echo "  - Checking Docker compliance..."
+./inspect.sh docker
+echo "  - Checking Kubernetes compliance..."
+./inspect.sh kubernetes
