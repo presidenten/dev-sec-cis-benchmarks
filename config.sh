9bd182b627f0899dd8482cdb634d90c049f4a250
Johan Hernefeldt <johan.hernefeldt@teliacompany.com>
2019-11-18 18:23:15 +0100

    Updated to newest docker benchmark commit hash

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ export KUBERNETES_VERSION=v1.4.1
 export KUBERNETES_HASH=66dabc1
 
 export DOCKER_VERSION=v1.13.0
-export DOCKER_HASH=4625e87
+export DOCKER_HASH=dcdcf34
 
 export DIL_VERSION=v1.0.1
 export DIL_HASH=52ed9db
